18dba85e06e53610cc242b2f3252e73be95d02d2
Diogo Rangel Dos Santos <diodam.rangel@gmail.com>
2025-05-14 09:59:04 -0300

    versao week 01

---
+++ week 01/scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display the last modified date
+document.getElementById("lastModified").textContent = `Last modification: ${document.lastModified}`;
